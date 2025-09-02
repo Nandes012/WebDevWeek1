fe2e2c9d616d9c0b38b250a3b795a4ee500898b3
Fernandes Howard <fernandeshoward2005@gmail.com>
2025-09-02 14:18:06 +0800

    Adding More Example

--- Praktikum/Week1/index.php
+++ Praktikum/Week1/index.php
@@ -27,9 +27,20 @@ $nums1 = [1,2,3,0,0,0];
 $m = 3;
 $nums2 = [2,5,6];
 $n = 3;
-
 merge($nums1, $m, $nums2, $n);
+echo "[" . implode(", ", $nums1) . "]<br>";
 
-// Print in clean format
-echo "[" . implode(", ", $nums1) . "]\n";
+$nums1 = [1];
+$m = 1;
+$nums2 = [];
+$n = 0;
+merge($nums1, $m, $nums2, $n);
+echo "[" . implode(", ", $nums1) . "]<br>";
+
+$nums1 = [0];
+$m = 0;
+$nums2 = [1];
+$n = 1;
+merge($nums1, $m, $nums2, $n);
+echo "[" . implode(", ", $nums1) . "]<br>";
 ?>
